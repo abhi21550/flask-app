0d492277fbd88e598acb0a2ed788c35a24c92c9b
Abhinand <110587537+abhi21550@users.noreply.github.com>
2024-04-27 21:13:42 +0530

    Add files via upload

---
+++ application/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "996bf76dae69e6da2cf1287d083f74a352c77b21"
+#app.config["MONGO_URI"] = "mongodb+srv://abhinandmukundan15:<abhinandmukundan15>@cluster0.nttfi0k.mongodb.net/"
+
+app.config["MONGO_URI"]="mongodb://localhost:27017/todo"
+# mongodb database
+mongodb_client = PyMongo(app)
+db = mongodb_client.db
+
+from application import routes
